8ba4c4df2c69b55adea2b32e703fc2583a41d966
Felipe Alves <felipealves131351@gmail.com>
2022-09-20 20:44:11 -0300

    configurando firebase e rotas

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './singin.css'
+
+function SignIn() {
+
+  
+  return (
+    <div>
+      <h1>PAGINA DE LOGIN</h1>
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBP0d5rLNkxeJmXgAz1WDIAr0Sj78vDKmA",
+  authDomain: "sistema-af01c.firebaseapp.com",
+  projectId: "sistema-af01c",
+  storageBucket: "sistema-af01c.appspot.com",
+  messagingSenderId: "152522299893",
+  appId: "1:152522299893:web:762bf1bc93faf45a2168bc",
+  measurementId: "G-6YW4YBD31G"
+};
+
+// Initialize Firebase
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
